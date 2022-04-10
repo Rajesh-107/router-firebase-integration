28f4911ec5a79908a11ffaab4316c99890c0fb8a
Rajesh-107 <applerb326@gmail.com>
2022-04-10 11:32:04 +0600

    handle requireauth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,29 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 import app from '../../firebase.init';
 import './Login.css';
 
 const auth = getAuth(app);
 const Login = () => {
     const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+    const location = useLocation();
+    const  navigate = useNavigate();
+
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+        .then( ()=> {
+            navigate(from, {replace:true})
+        })
+    }
     return (
         <div>
             <h2>This is login part</h2>
             <div style={{margin:'20px'}}>
-                <button onClick={ () => signInWithGoogle()}>Google Sign in</button>
+                <button onClick={ handleGoogleSignIn}>Google Sign in</button>
             </div>
             <form>
             <input type="email" name="" id="" placeholder='enter your email' />
---
+++ src/components/Order/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>Thsi is orders</h2>
+        </div>
+    );
+};
+
+export default Orders;
